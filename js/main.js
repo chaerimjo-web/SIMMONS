8e9bc4dc4dc267de97f4b35efeb5f6250d094aa4
chaerimjo-web <chaerim010710@gmail.com>
2024-08-21 16:49:04 +0900

    upload

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     const goTop = document.querySelector('#go-top');
     
     window.addEventListener('scroll',()=>{
-      let scrollAmt = window.scrollY;
+      let scrollAmt = window.scrollY; 
       console.log(scrollAmt);
     
       if(scrollAmt > 600){
