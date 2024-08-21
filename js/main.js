de147b6a964b6607f3f1010679b18cb15f3417dd
chaerimjo-web <chaerim010710@gmail.com>
2024-08-22 06:05:20 +0900

    팝업

--- js/main.js
+++ js/main.js
@@ -145,3 +145,42 @@ for(let pdtm of tabMenu){
 
 
         
+const popup = document.querySelector('.popup');
+const check = document.querySelector('#check');
+const button = document.querySelector('button');
+
+button.addEventListener('click',()=>{
+  if(check.checked){
+    //쿠키생성
+    setCookie('Company','ABC',1);
+  } else{
+    //쿠키제거
+    delCookie('Company','ABC');
+  }
+  popup.classList.remove('show');
+});
+
+function setCookie(name,val,due){
+  let date = new Date();
+    date.setDate(date.getDate() + due);
+
+    let myCookie = `${name}=${val};expires=`+date.toUTCString();
+    document.cookie = myCookie;
+}
+
+function delCookie(name,val){
+  let date = new Date();
+    date.setDate(date.getDate() -1);
+
+    let myCookie = `${name}=${val};expires=`+date.toUTCString();
+    document.cookie = myCookie;
+}
+
+//Company=ABC, checkCookie
+function checkCookie(name,val){
+  if(document.cookie.search(`${name}=${val}`) === -1){
+    popup.classList.add('show');
+  }
+}
+
+checkCookie('Company','ABC');
